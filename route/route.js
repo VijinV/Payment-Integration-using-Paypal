d4160018a298d65d1bb73b8300593e697456054d
vijinV <vfcvijin@gmail.com>
2023-07-19 17:11:51 -0400

    Payment integration with paypal

--- route/route.js
+++ route/route.js
@@ -11,7 +11,22 @@ route.post('/paypal',controller.paypalPayment);
 
 
 route.get('/paypalsuccess',(req,res)=>{
-    res.send('success');
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const executePayment = {
+      payer_id: payerId,
+    };
+  
+    paypal.payment.execute(paymentId, executePayment, (error, payment) => {
+      if (error) {
+        console.error('Error executing PayPal payment:', error);
+        res.redirect('/paypalcancel');
+      } else {
+        
+        res.send('Payment Success'); 
+      }
+    });
 })
 
 
